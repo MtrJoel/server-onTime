29d1b0874afa22781294223deeb6bb43389c828d
Mtrjoel <monterojoel135@gmail.com>
2025-06-17 18:01:05 -0400

    r

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+// require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const PORT = process.env.PORT || 3000;
+
 
 const app = express();
 app.use(cors());
@@ -9,11 +16,11 @@ app.use(express.json());
 
 app.use('/api', require('./routes/authRoutes'));
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(PORT)
   .then(() => {
     console.log('✅ Conectado a MongoDB');
     app.listen(process.env.PORT, () => {
-      console.log(`🚀 Servidor corriendo en http://localhost:${process.env.PORT}`);
+      console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
     });
   })
   .catch(err => console.error('❌ Error al conectar con MongoDB', err));
